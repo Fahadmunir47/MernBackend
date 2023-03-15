357bdd2a1a60359a3fb399e19f79b5bf5d042fc3
Fahad <fahadmunir922@gmail.com>
2023-03-15 16:56:53 +0500

    app updated

--- src/app.js
+++ src/app.js
@@ -85,6 +85,18 @@ app.post("/login", async (req, res) => {
   }
 });
 
+const bcrypt = require("bcryptjs");
+
+const securePassword = async (password) => {
+  const passwordHash = await bcrypt.hash(password, 10);
+  console.log(passwordHash);
+
+  const passwordmatch = await bcrypt.compare("fahad@123", passwordHash);
+  console.log(passwordmatch);
+};
+
+securePassword("fahad@123");
+
 app.listen(port, () => {
   console.log(`server is running at port no. ${port}`);
 });
