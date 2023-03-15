aade254834a708241eb1ef1facca4c470014c8ab
Fahad <fahadmunir922@gmail.com>
2023-03-15 17:49:45 +0500

    registers updated

--- src/models/registers.js
+++ src/models/registers.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const employeeSchema = new mongoose.Schema({
   firstname: {
@@ -35,6 +36,15 @@ const employeeSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+employeeSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+
+    this.confirmpassword = undefined;
+  }
+  next();
 });
 
 const Register = new mongoose.model("Register", employeeSchema);
